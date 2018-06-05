d3f1c1d78c5c197aac4c8ec1b0331dc39fac10a6
Jantz <jantzillac.93@gmail.com>
2018-06-05 11:58:01 -0500

    Add a project dependence between your app and JokeWizard

--- JokeWizard/src/main/java/com/creativesourceapps/android/jokewizard/GetJoke.java
+++ JokeWizard/src/main/java/com/creativesourceapps/android/jokewizard/GetJoke.java
@@ -1,4 +1,12 @@
 package com.creativesourceapps.android.jokewizard;
 
+import com.creativesourceapps.java.joke.JokeSmith;
+
 public class GetJoke {
+
+    public String getJoke() {
+        JokeSmith jokeSmith = new JokeSmith();
+
+        return jokeSmith.tellAHandCraftedJoke();
+    }
 }
