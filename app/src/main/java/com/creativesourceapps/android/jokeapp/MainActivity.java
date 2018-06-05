dbe21b35fd49027212f13284a92ac538374821c0
Jantz <jantzillac.93@gmail.com>
2018-06-05 11:40:00 -0500

    Add a text view to MainActivity and populate it with a joke from JokeSmith.

--- app/src/main/java/com/creativesourceapps/android/jokeapp/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/jokeapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.creativesourceapps.android.jokeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    JokeSmith jokeSmith;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jokeSmith = new JokeSmith();
+
+        textView.setText(jokeSmith.tellAHandCraftedJoke());
     }
 }
