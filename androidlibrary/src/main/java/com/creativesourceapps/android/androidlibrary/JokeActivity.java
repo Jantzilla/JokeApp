8438b40225fc45b650cf17a8652afb5f93f177b4
Jantz <jantzillac.93@gmail.com>
2018-06-05 17:02:38 -0500

    Have the activity in the Android Library retrieve and display the joke.

--- androidlibrary/src/main/java/com/creativesourceapps/android/androidlibrary/JokeActivity.java
+++ androidlibrary/src/main/java/com/creativesourceapps/android/androidlibrary/JokeActivity.java
@@ -2,12 +2,19 @@ package com.creativesourceapps.android.androidlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+        textView = (TextView) findViewById(R.id.text_view_three);
+
+        String joke = getIntent().getStringExtra("joke");
+
+        textView.setText(joke);
     }
 }
