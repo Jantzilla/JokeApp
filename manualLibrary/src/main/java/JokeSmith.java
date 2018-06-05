13f671a51ae1b910511a90169e4273516e5ae294
Jantz <jantzillac.93@gmail.com>
2018-06-05 11:34:13 -0500

    Add closing "}" to JokeSmith class

--- manualLibrary/src/main/java/JokeSmith.java
+++ manualLibrary/src/main/java/JokeSmith.java
@@ -1,6 +1,7 @@
 import java.lang.String;
 
-public class JokeSmith{
-    public String tellAHandCraftedJoke(){
+public class JokeSmith {
+    public String tellAHandCraftedJoke() {
         return "Why did the chicken cross the road? \n \n A hot chick!";
     }
+}
