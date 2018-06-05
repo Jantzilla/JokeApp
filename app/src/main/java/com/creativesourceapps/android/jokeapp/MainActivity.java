f019c866f5fb321ac4d9be25c50b77e9b63cb201
Jantz <jantzillac.93@gmail.com>
2018-06-05 12:00:27 -0500

    Add another text view to MainActivityFragment and populate it with a joke from JokeWizard.

--- app/src/main/java/com/creativesourceapps/android/jokeapp/MainActivity.java
+++ app/src/main/java/com/creativesourceapps/android/jokeapp/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.creativesourceapps.android.jokewizard.GetJoke;
 import com.creativesourceapps.java.joke.JokeSmith;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView, textView2;
     JokeSmith jokeSmith;
+    GetJoke getJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         jokeSmith = new JokeSmith();
+        getJoke = new GetJoke();
+
 
         textView.setText(jokeSmith.tellAHandCraftedJoke());
+
+        textView2.setText(getJoke.getJoke());
     }
 }
