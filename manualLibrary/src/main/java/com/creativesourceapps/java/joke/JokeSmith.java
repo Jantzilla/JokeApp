3a1032553f52edc9ce851f55717fe57102f7993a
Jantz <jantzillac.93@gmail.com>
2018-06-07 10:23:48 -0500

    Remove "manualLibrary"

--- manualLibrary/src/main/java/com/creativesourceapps/java/joke/JokeSmith.java
+++
@@ -1,9 +0,0 @@
-package com.creativesourceapps.java.joke;
-
-import java.lang.String;
-
-public class JokeSmith {
-    public String tellAHandCraftedJoke() {
-        return "Why did the chicken cross the road? \n \n A hot chick!";
-    }
-}
